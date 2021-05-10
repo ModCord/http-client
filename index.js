db96ea21c0bd360cc6f710e4c08d5a1937f95e9b
MrAugu <mraugu@yahoo.com>
2021-05-10 07:28:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-module.exports = require("./src/index.js");
+const { Request, Response } require("./src/index.js");
+
+module.exports = {
+  request: (...args) => return new Request(...args),
+  Request,
+  Response
+}
